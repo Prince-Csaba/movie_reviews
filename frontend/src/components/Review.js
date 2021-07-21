e37d3ab5578e630974fa80078b02c07acfde53de
Bian Péter <bianpeti@gmail.com>
2021-07-21 16:22:00 +0200

    review.js content

--- frontend/src/components/Review.js
+++ frontend/src/components/Review.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
+import ReviewCard from './ReviewCard';
 
 function Review() {
   const [radio, setRadio] = useState("movie-title")
   const [search, setSearch] = useState("")
+  const [review, setReview] = useState(null)
 
   let reviewData = {
     searchValue: search,
@@ -14,7 +16,8 @@ function Review() {
     axios
       .post('http://localhost:8080/api/getReview', reviewData)
       .then((res) => {
-        console.log(res)
+        setReview(res.data)
+        console.log(res.data)
       })
       .catch((err) => console.log(err.response));
   }
@@ -29,6 +32,9 @@ function Review() {
         <input type="radio" name="searchBy" value="movie-title" onChange={() => setRadio("movie-title")} checked={radio === "movie-title"} ></input>
         <label htmlFor="user">Search by Users</label>
         <input type="radio" name="searchBy" value="user" onChange={() => setRadio("user")} checked={radio === "user"} ></input>
+        {
+          review && review.map((review,i) => <ReviewCard review={review} key={i}/>)
+        }
       </div>
     </div>
   )
