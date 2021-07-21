649cc895e773656cfc856f6853aa7c602c12c6ab
Bian Péter <bianpeti@gmail.com>
2021-07-21 16:38:36 +0200

    backend code fix

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,10 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
-//const request = require('request');
 const allowCors = require('./middleware/allowCors');
 const corsOptions = require('./utils/corsOptions');
 const notExists = require('./middleware/notExists');
-
-const { googleLogin } = require('./controllers/LoginController');
-const { commentReview } = require('./controllers/ReviewController')
-const { getMovie } = require('./controllers/MovieController')
-const { getReview } = require('./controllers/GetReviewController')
+const mainRoute = require('./routes/main');
 
 
 // Init Middleware
@@ -24,16 +19,7 @@ app.get('/', (req, res) => {
 	res.send('Backend is on')
 });
 
-//GET - Some kind of movie
-//Public
-app.get('/movie/:movie', getMovie);
-
-//login try (based on Riki)
-app.post("/api/login", googleLogin)
-
-app.post("/api/review", commentReview)
-
-app.post("/api/getReview", getReview)
+app.use('/', mainRoute);
 
 app.use(notExists);
 
--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const { googleLogin } = require('../controllers/LoginController');
 const { commentReview } = require('../controllers/ReviewController');
+const { getReview } = require('../controllers/GetReviewController')
+const { getMovie } = require('../controllers/MovieController')
 
-router.post('/login', googleLogin);
+router.get('/movie/:movie', getMovie);
 
-router.post("/review", commentReview)
+router.post('/api/login', googleLogin);
+
+router.post("/api/review", commentReview)
+
+router.post("/api/getReview", getReview)
+
+module.exports = router;
