9a6d6eb18c40291a0f42ff5fa7684079be549ea2
ExceedPrince <tamas.bodorkos@hotmail.hu>
2021-07-22 17:19:51 +0200

    backend tests

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -108,6 +108,7 @@ describe("testing some easy case", () => {
 		const review = await Review.findOne({});
 
 		expect(res.status).toBe(200);
+		expect(res.body).toBe("Success!");
 		expect(typeof review).toBe('object');
 		expect(review.google_id).toBe("google123456");
 		expect(review.full_name).toBe("Kandisz Nóra");
@@ -117,4 +118,107 @@ describe("testing some easy case", () => {
 		expect(review._id).not.toBeUndefined();
 		expect(review.date).not.toBeUndefined();
 	});
+
+
+	test("If searchValue=super, and radioValue=movie-title, then return 'Superman' ", async () => {
+		//insert 2 reviews
+		await Review.insertMany([{
+			google_id: "456", full_name: "Para Zita",
+			movie_title: "Superman", movie_id: "01", comment: "Volt már jobb is"
+		},
+		{
+			google_id: "137", full_name: "Beviz Elek",
+			movie_title: "Batman", movie_id: "02", comment: "Inkább Pókember"
+		}]);
+
+		const reviewArr = await Review.find();
+
+		//POST 'super, movie-title'
+		const res = await request.post("/api/getReview").send({ searchValue: "super", radioValue: "movie-title" });
+
+		expect(reviewArr).toHaveLength(2);
+		expect(res.status).toBe(200);
+		expect(typeof res.body).toBe('object');
+		expect(res.body.length).toBe(1);
+		expect(res.body[0]).toHaveProperty('google_id', "456");
+		expect(res.body[0]).toHaveProperty('full_name', "Para Zita");
+		expect(res.body[0]).toHaveProperty('movie_title', "Superman");
+		expect(res.body[0]).toHaveProperty('movie_id', "01");
+		expect(res.body[0]).toHaveProperty('comment', "Volt már jobb is");
+	});
+
+
+	test("If searchValue=viz, and radioValue=user, then return 'Beviz Elek' ", async () => {
+		//insert 2 reviews
+		await Review.insertMany([{
+			google_id: "456", full_name: "Para Zita",
+			movie_title: "Superman", movie_id: "01", comment: "Volt már jobb is"
+		},
+		{
+			google_id: "137", full_name: "Beviz Elek",
+			movie_title: "Batman", movie_id: "02", comment: "Inkább Pókember"
+		}]);
+
+		const reviewArr = await Review.find();
+
+		//POST 'viz, user'
+		const res = await request.post("/api/getReview").send({ searchValue: "viz", radioValue: "user" });
+
+		expect(reviewArr).toHaveLength(2);
+		expect(res.status).toBe(200);
+		expect(typeof res.body).toBe('object');
+		expect(res.body.length).toBe(1);
+		expect(res.body[0]).toHaveProperty('google_id', "137");
+		expect(res.body[0]).toHaveProperty('full_name', "Beviz Elek");
+		expect(res.body[0]).toHaveProperty('movie_title', "Batman");
+		expect(res.body[0]).toHaveProperty('movie_id', "02");
+		expect(res.body[0]).toHaveProperty('comment', "Inkább Pókember");
+	});
+
+
+	test("If searchValue=ádám, and radioValue=user, then return nothing ", async () => {
+		//insert 2 reviews
+		await Review.insertMany([{
+			google_id: "456", full_name: "Para Zita",
+			movie_title: "Superman", movie_id: "01", comment: "Volt már jobb is"
+		},
+		{
+			google_id: "137", full_name: "Beviz Elek",
+			movie_title: "Batman", movie_id: "02", comment: "Inkább Pókember"
+		}]);
+
+		const reviewArr = await Review.find();
+
+		//POST 'ádám, user'
+		const res = await request.post("/api/getReview").send({ searchValue: "ádám", radioValue: "user" });
+
+		expect(reviewArr).toHaveLength(2);
+		expect(res.status).toBe(200);
+		expect(typeof res.body).toBe('object');
+		expect(res.body).toEqual([]);
+	});
+
+
+	test("If searchValue=batman, and radioValue=user, then return nothing ", async () => {
+		//insert 2 reviews
+		await Review.insertMany([{
+			google_id: "456", full_name: "Para Zita",
+			movie_title: "Superman", movie_id: "01", comment: "Volt már jobb is"
+		},
+		{
+			google_id: "137", full_name: "Beviz Elek",
+			movie_title: "Batman", movie_id: "02", comment: "Inkább Pókember"
+		}]);
+
+		const reviewArr = await Review.find();
+
+		//POST 'batman, user'
+		const res = await request.post("/api/getReview").send({ searchValue: "batman", radioValue: "user" });
+
+		expect(reviewArr).toHaveLength(2);
+		expect(res.status).toBe(200);
+		expect(typeof res.body).toBe('object');
+		expect(res.body).toEqual([]);
+	});
+
 });
